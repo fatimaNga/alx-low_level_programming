e22b96196ba7a01ef271229af17a2ccd35f75f52
fatimaNga <teemsha002@gmail.com>
2022-09-17 13:10:04 -0700

    My first functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - programe entry point
+ *
+ * Description: this is a program to print putcher
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
