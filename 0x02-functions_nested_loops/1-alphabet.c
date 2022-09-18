0c6c0573d2cb2f53c07868d90c015ce90defbda8
fatimaNga <teemsha002@gmail.com>
2022-09-18 03:32:44 -0700

    My second function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,11 @@
  */
 void print_alphabet(void)
 {
-     char c;
+   char c;
 
-    
-     for (c = 'a'; c <= 'z'; c++)
+   for (c ='a'; c <= 'z'; c++)
      {
-    	 _putchar(c);
+            _putchar(c);
      }
      _putchar('\n');
 }     
