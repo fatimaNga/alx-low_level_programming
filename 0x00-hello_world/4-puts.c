ae7a5a886a07cb83f5ed56f083aeeb20fa55a547
fatimaNga <teemsha002@gmail.com>
2022-09-21 14:36:41 -0700

    My fifth low level

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main- Serves as the entry point for the program
+ * main - Entry point
  *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
