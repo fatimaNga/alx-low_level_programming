de95abfe5dffc196868220b655c47bb4768b9c80
fatimaNga <teemsha002@gmail.com>
2022-09-21 18:02:51 -0700

    My seventh low level

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-	char acharacter;
-	int ainteger;
-	long along;
-	long long alonglong;
-	float afloat;
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 
-	printf("size of a char:%lu byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %lu byte(s)\n", sizeof(afloat));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
 	return (0);
 }
